eeecc6d22b5750b12e05cc8bef0ec07f17e15b66
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-10-11 11:19:22 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+  my_hash = {name: "Nick", height: "75 inches", age: 25, net_worth: "$90,000"}
   
 end
 
